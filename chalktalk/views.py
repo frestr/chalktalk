c255ff282ba3352f11612bb468194df19f31d770
Thomas <thomskar@stud.ntnu.no>
2017-02-14 22:32:10 +0100

    Added lecturer.html, also added a css skeleton for a calendar aaswell as it's correpsonding html

--- chalktalk/views.py
+++ chalktalk/views.py
@@ -1,7 +1,14 @@
+from pip._vendor.requests.packages.urllib3 import request
+
 from chalktalk import app
-from flask import render_template
+from flask import render_template, request, url_for, redirect
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
+
+
+@app.route('/lecturer')
+def lecturer():
+    return render_template('lecturer.html')
