901d444e244879c5aec1989d7ea3371fb40066c8
junglebooboo <aigtum@gmail.com>
2017-02-16 11:11:02 +0100

    Fixed save form

--- chalktalk/feedbackforms.py
+++ chalktalk/feedbackforms.py
@@ -3,6 +3,6 @@ from wtforms import TextAreaField, RadioField, SubmitField
 
 
 class Feedbackform(FlaskForm):
-    rating = RadioField('Hvor mye forstod du av ', choices=[(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)])
-    comment = TextAreaField("Eventuelle kommentarer om temaet:")
+    rating = RadioField('Forståelse', choices=[(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)])
+    comment = TextAreaField("Kommentar")
     submit = SubmitField("Lagre")
