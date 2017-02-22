368f6dda6884ea86b087932242ef6dfbb1a935da
Fredrik Strupe <fredrik@strupe.net>
2017-02-22 20:24:39 +0100

    Preliminary feedback registration

--- chalktalk/feedbackforms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import TextAreaField, RadioField, SubmitField
-
-
-class Feedbackform(FlaskForm):
-    rating = RadioField('Forståelse', choices=[(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)])
-    comment = TextAreaField("Kommentar")
-    submit = SubmitField("Lagre")
--- chalktalk/views.py
+++ chalktalk/views.py
@@ -1,12 +1,13 @@
 from chalktalk import app
 from flask import render_template, request, url_for, redirect, abort
-from .feedbackforms import Feedbackform
+from chalktalk.feedbackforms import Feedbackform
 import chalktalk.database
 
 database_url = 'sqlite:///dummy.db'
 db = chalktalk.database.DatabaseManager(database_url)
 
 app.secret_key = 'development key'
+
 
 @app.route('/')
 def index():
@@ -17,20 +18,36 @@ def index():
 def lecturelist():
     return render_template('lecturelist.html')
 
+
 @app.route('/createlecturelist')
 def createlecturelist():
     return render_template('createlecturelist.html')
 
-@app.route('/feedback/', methods=['post', 'get'])
-def feedbackform():
-    lecture = "TDT4100"
-    subjects = ['Pastasaus', 'Pizzabunn', 'Algoritmer']
-    form = Feedbackform()
+
+@app.route('/feedback/<lecture_id>', methods=['POST', 'GET'])
+def feedbackform(lecture_id):
+    lecture = db.session.query(chalktalk.database.Lecture).filter_by(id=lecture_id).first()
+    if lecture is None:
+        abort(404)
+    subjects = db.session.query(chalktalk.database.Subject).filter_by(lecture_id=lecture.id).all()
 
     if request.method == 'POST':
-        return 'Form posted.'
-    elif request.method == 'GET':
-        return render_template('feedbackform.html', form=form, lecture=lecture, subjects=subjects)
+        for subject in subjects:
+            keys = ['{}_keyword'.format(subject.id),
+                    '{}_rating'.format(subject.id),
+                    '{}_comment'.format(subject.id)]
+            if (keys[0] in request.form and
+                    keys[1] in request.form and
+                    keys[2] in request.form):
+                print(request.form[keys[0]],
+                      request.form[keys[1]],
+                      request.form[keys[2]])
+
+    return render_template('feedbackform.html',
+                           lecture_id=lecture_id,
+                           lecture=lecture,
+                           subjects=subjects)
+
 
 @app.route('/lecturefeedback/<lecture_id>')
 def lecturefeedback(lecture_id):
@@ -40,6 +57,7 @@ def lecturefeedback(lecture_id):
     subjects = db.get_subject_values(lecture)
     print(subjects)
     return render_template('lecturefeedback.html', subjects=subjects)
+
 
 @app.route('/lecturertest')
 def lecturertest():
