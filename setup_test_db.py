6bf1a66b49b9e7d046ba0305f2d3f8ad991ced7d
Fredrik Strupe <fredrik@strupe.net>
2017-03-23 14:13:46 +0100

    Add more lectures to test db

--- setup_test_db.py
+++ setup_test_db.py
@@ -32,13 +32,33 @@ db.add_lecturer_to_course(lecturer1, course3)
 db.add_lecturer_to_course(lecturer2, course4)
 
 # Add lectures and some subjects
-lecture1 = db.add_lecture(course1, datetime.now(), 'MTDT', [lecturer1])
+lecture1 = db.add_lecture(course1,
+                          datetime(2017, 3, 10, 14, 15),
+                          'MTDT',
+                          [lecturer1])
 subject1 = db.add_subject(lecture1, 'Programvare')
 subject2 = db.add_subject(lecture1, 'Utvikling')
 
-lecture2 = db.add_lecture(course1, datetime.now(), 'MTDT', [lecturer1])
+lecture2 = db.add_lecture(course1,
+                          datetime(2017, 3, 14, 12, 15),
+                          'MTDT',
+                          [lecturer1])
 subject3 = db.add_subject(lecture2, 'Java')
 subject4 = db.add_subject(lecture2, 'Python')
+
+lecture3 = db.add_lecture(course1,
+                          datetime(2017, 3, 18, 14, 15),
+                          'MTDT',
+                          [lecturer1])
+subject5 = db.add_subject(lecture3, 'SEMAT')
+subject6 = db.add_subject(lecture3, 'Scrum')
+
+lecture4 = db.add_lecture(course1,
+                          datetime(2017, 3, 20, 12, 15),
+                          'MTDT',
+                          [lecturer1])
+subject7 = db.add_subject(lecture4, 'Pekka')
+subject8 = db.add_subject(lecture4, 'Testing')
 
 # Add feedback to the lecture and its subjects
 feedback1 = db.add_lecture_feedback(student, lecture1, 'BAD!')
@@ -52,5 +72,13 @@ db.add_subject_feedback(feedback1, subject2, 4, 'Hard')
 feedback2 = db.add_lecture_feedback(student, lecture2, 'Good!')
 db.add_subject_feedback(feedback2, subject3, 3, 'java is hard')
 db.add_subject_feedback(feedback2, subject4, 5, 'python is ezpz')
+
+feedback3 = db.add_lecture_feedback(student, lecture3, 'Good!')
+db.add_subject_feedback(feedback3, subject5, 1, 'I had some trouble')
+db.add_subject_feedback(feedback3, subject6, 3, 'Nice explanation')
+
+feedback4 = db.add_lecture_feedback(student, lecture4, 'Good!')
+db.add_subject_feedback(feedback4, subject7, 5, 'Awesome!')
+db.add_subject_feedback(feedback4, subject8, 4, 'Ok i guess')
 
 db.save_changes()
